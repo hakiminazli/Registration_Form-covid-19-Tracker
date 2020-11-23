fa4f66a5f3fd528efd9fc82b6912d9931876d182
hakiminazli <67014568+hakiminazli@users.noreply.github.com>
2020-11-24 01:04:32 +0800

    This is the first version or prototype for Registration form

---
+++ android/app/src/main/kotlin/com/example/register_covid19app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.register_covid19app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class SignupScreen extends StatefulWidget {
+  static const routeName = '/signup';
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+
+  final GlobalKey<FormState> _formkey = GlobalKey();
+  TextEditingController _passwordController = new TextEditingController();
+
+  void _submit()
+  {
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+
+        actions: <Widget>[
+          FlatButton(
+            child: Row(
+              children: <Widget>[
+                Text('Login'),
+                Icon(Icons.person)
+              ],
+            ),
+            textColor: Colors.white,
+            onPressed: ()
+            {
+              Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.limeAccent,
+                      Colors.redAccent,
+                    ]
+                )
+            ),
+          ),
+          Center(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Container(
+                height: 300,
+                width: 300,
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formkey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        //email
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Email'),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value)
+                          {
+                            if(value.isEmpty || !value.contains('@'))
+                            {
+                              return 'Invalid Email!';
+                            }
+                            return null;
+                          },
+                          onSaved: (value)
+                          {
+
+                          },
+                        ),
+
+                        //password
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                          controller: _passwordController,
+                          validator: (value)
+                          {
+                            if(value.isEmpty || value.length <= 5)
+                            {
+                              return 'invalid password';
+                            }
+                            return null;
+                          },
+                          onSaved: (value)
+                          {
+
+                          },
+                        ),
+
+                        //Confirm Password
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Confirm Password'),
+                          obscureText: true,
+                          validator: (value)
+                          {
+                            if(value.isEmpty || value != _passwordController.text)
+                            {
+                              return 'invalid password';
+                            }
+                            return null;
+                          },
+                          onSaved: (value)
+                          {
+
+                          },
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        RaisedButton(
+                          child: Text(
+                              'Submit'
+                          ),
+                          onPressed: ()
+                          {
+                            _submit();
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
